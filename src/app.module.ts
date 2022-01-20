d9389554a8fc51eadc403326302852b2836504c6
Andy Primawan <andy@primawan.com>
2022-01-20 19:32:48 +0700

    skeleton of users domain, load to app,module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { ProjectsController } from './projects/projects.controller';
+import { HeadlinesController } from './headlines/headlines.controller';
+import { ControlsController } from './controls/controls.controller';
+import { NotesController } from './notes/notes.controller';
+import { DocumentsController } from './documents/documents.controller';
+import { TodosController } from './todos/todos.controller';
+import { ExportsController } from './exports/exports.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ],
+  controllers: [AppController, ProjectsController, HeadlinesController, ControlsController, NotesController, DocumentsController, TodosController, ExportsController],
   providers: [AppService],
 })
 export class AppModule {}
